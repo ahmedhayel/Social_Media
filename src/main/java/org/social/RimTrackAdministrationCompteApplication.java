b49d9cefdc42ea1e7c33efb7ed3c470845e9ce31
ahmed <ahmed0hayel@gmail.com>
2017-05-25 14:23:57 +0000

    begin commit

---
+++ src/main/java/org/social/RimTrackAdministrationCompteApplication.java
@@ -0,0 +1,12 @@
+package org.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RimTrackAdministrationCompteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RimTrackAdministrationCompteApplication.class, args);
+	}
+}
